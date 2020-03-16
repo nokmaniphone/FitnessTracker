47ce5c30040450e7718127e70461de7bf897a5e0
nokmaniphone <nokmaniphonesayavong@gmail.com>
2020-03-16 00:39:43 -0700

    change the heroku db

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect('mongodb://localhost/workout', {
+module.exports = require('mongoose').connect('mongodb://heroku_5jk0qb3l:tdk0ii6cg6dv0rtmv4j5sh9c6g@ds047325.mlab.com:47325/heroku_5jk0qb3l', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
