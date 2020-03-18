f9b463f015b382cb993d1cd2b8d8ce62167aa01c
nokmaniphone <nokmaniphonesayavong@gmail.com>
2020-03-17 22:47:40 -0700

    fixed sever.js

--- config/index.js
+++ config/index.js
@@ -4,3 +4,5 @@ module.exports = require('mongoose').connect(process.env.MONGODB_URL || process.
   useFindAndModify: false
 })
 
+// mongodb://heroku_5jk0qb3l:tdk0ii6cg6dv0rtmv4j5sh9c6g@ds047325.mlab.com:47325/heroku_5jk0qb3l
+//process.env.MONGODB_URL
