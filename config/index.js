1f96f8f1181f8b8e2c3cfbdb0fcd13233a46a9b6
nokmaniphone <nokmaniphonesayavong@gmail.com>
2020-03-17 23:06:21 -0700

    fixed sever.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = require('mongoose').connect(process.env.MONGODB_URL || process.env.LOCAL_URI, {
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
